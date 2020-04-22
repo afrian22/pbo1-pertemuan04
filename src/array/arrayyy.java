edd38e6526901ba2f83eed6811420aaecd3bb5b4
afrian22 <62339501+afrian22@users.noreply.github.com>
2020-04-22 20:34:00 +0800

    arayyyyyyyy

---
+++ src/array/arrayyy.java
@@ -0,0 +1,21 @@
+package array;
+
+public class arrayyy {
+        public static void main(String[] args) {
+
+                String mataKuliah[];
+                mataKuliah = new String[10];
+
+                mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+                mataKuliah[1] = "Visual2";
+                mataKuliah[2] = "Rekayasa Perangkat Lunak";
+                mataKuliah[3] = "Main Game";
+                mataKuliah[4] = "Web";
+                mataKuliah[5] = "SDB";
+                mataKuliah[6] = "PBO";
+                mataKuliah[7] = "Visual1";
+                mataKuliah[8] = "Web2";
+                mataKuliah[9] = "SOSBUD";
+        }
+    }
+}
