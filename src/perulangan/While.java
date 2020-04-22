f839e231700378e4691e15dc6c27df1c68b2906b
afrian22 <62339501+afrian22@users.noreply.github.com>
2020-04-22 19:46:08 +0800

    perulangan while

--- src/perulangan/While.java
+++ src/perulangan/While.java
@@ -1,6 +1,6 @@
 package perulangan;
 
-public class perulanganwhile {
+public class While {
     public static void main(String[] args) {
         int isiGelas = 0;
         int penuh = 225;
@@ -8,12 +8,11 @@ public class perulanganwhile {
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "m1");
 
-        while(isiGelas != penuh){
+        while (isiGelas != penuh){
             isiGelas++;
             System.out.println("Sedang Mengisi Gelas..");
-            System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas "m1");
         }
-        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas "m1");
+        System.out.println("Finale: Isi Gelas Sekarang : " +  isiGelas "m1");
     }
-
 }
